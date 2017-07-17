1d722cf98bee0ec104389420899c57e9cba37397
makadaw <makadaw@gmail.com>
2017-07-17 16:40:27 +0200

    WIP

--- Sources/BotsKit/ConnectorDispatcher.swift
+++ Sources/BotsKit/ConnectorDispatcher.swift
@@ -42,34 +42,34 @@ internal final class BotConnector {
         queue = DispatchQueue(label: "Dispatch \(bot.name) - \(provider.name)", attributes: .concurrent)
         
         // Subscribe on signals
+        
+        // For many providers we need to answer ASAP with HTTP response, so for
+        // this need to schedule processing on other queue
         self.provider.recieveActivity.subscribe{ [unowned self] activity in
-            self.dispatch(incomeActivity: activity)
+            self.dispatch {
+                self.bot.dispatch(activity: activity)
+            }
         }
+        
+        // Dispatch output activity to provider
+        // This method can be run concurrently inside outcome group
         self.bot.sendActivity.subscribe{ [unowned self] activity in
-            self.provider.send(activity: activity)
+            self.dispatch {
+                self.provider.send(activity: activity)
+            }
         }
     }
     
-    /// For many providers we need to answer ASAP with HTTP response, so for
-    /// this need to schedule processing on other queue
+    /// Dispatch block on connector queue
+    /// For now we run each block when set them, but in future this method will be more smart to descide
+    /// how to run blocks
     ///
-    /// - Parameter activity: income activity
-    internal func dispatch(incomeActivity activity: Activity) {
+    /// - Parameter activityBlock: block to execute
+    internal func dispatch(_ activityBlock: @escaping () -> Void) {
         queue.async {
-            self.bot.dispatch(activity: activity)
+            activityBlock()
         }
     }
-    
-    /// Dispatch output activity to provider
-    /// This method can be run concurrently inside outcome group
-    ///
-    /// - Parameter activity: activity to send
-    internal func dispatch(outcomeActivity activity: Activity) {
-        queue.async {
-            self.provider.send(activity: activity)
-        }
-    }
-    
     
 }
 
